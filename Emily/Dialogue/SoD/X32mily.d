14dfb418b30ce51faa90f69cd25f9c60fd1d5ca9
Skitia <57140485+Skitia@users.noreply.github.com>
2022-12-10 19:32:24 +0000

    Add files via upload

--- Emily/Dialogue/SoD/X32mily.d
+++ Emily/Dialogue/SoD/X32mily.d
@@ -6,6 +6,49 @@ SAY @47
 IF ~~ EXIT 
 END 
 
+
+
+/* By all checks this will never fire. */
+IF ~AreaCheck("bd4700")
+    GlobalLT("bd_plot","global",570)~ THEN BEGIN kickout_1
+  SAY @37
+  IF ~~ THEN DO ~SetGlobal("x3mily_kicked_bd4700","global",1)
+                 SetGlobal("bd_joined","locals",0)~ EXIT
+END
+
+/* If kicked out in Korlasz's tomb */
+IF ~OR(2)
+      AreaCheck("bd0120")
+      AreaCheck("bd0130")
+    GlobalGT("bd_joined","locals",0)~ THEN BEGIN kickout_2
+  SAY @38
+  ++ @39 + kickout_3
+  ++ @40 + kickout_4
+END
+
+
+
+/* kicked out somewhere else (not bd4700.are in Avernus, not Korlasz's tomb) */
+IF ~GlobalGT("bd_joined","locals",0)~ THEN BEGIN kickout_5
+  SAY @43
+  + ~GlobalGT("bd_npc_camp_chapter","global",1)
+     GlobalLT("bd_npc_camp_chapter","global",5)
+     OR(2)
+       !Range("ff_camp",999)
+       NextTriggerObject("ff_camp")
+     !IsOverMe("X3mily")~ + @44 DO ~SetGlobal("bd_npc_camp","locals",1)~ + kickout_6
+  ++ @45 + kickout_3
+  ++ @40 + kickout_4
+END
+
+
+
+IF ~~ THEN BEGIN kickout_6
+  SAY @46
+  IF ~~ THEN DO ~SetGlobal("bd_joined","locals",0)~ EXIT
+END
+
+
 IF ~Global("X3milyNeverJoined","locals",1)~ THEN BEGIN X3milyNeverJoined 
 SAY @51
 ++ @92 + kickout_4
@@ -174,47 +217,6 @@ END
 
 
 
-
-
-/* By all checks this will never fire. */
-IF ~AreaCheck("bd4700")
-    GlobalLT("bd_plot","global",570)~ THEN BEGIN kickout_1
-  SAY @37
-  IF ~~ THEN DO ~SetGlobal("x3mily_kicked_bd4700","global",1)
-                 SetGlobal("bd_joined","locals",0)~ EXIT
-END
-
-/* If kicked out in Korlasz's tomb */
-IF ~OR(2)
-      AreaCheck("bd0120")
-      AreaCheck("bd0130")
-    GlobalGT("bd_joined","locals",0)~ THEN BEGIN kickout_2
-  SAY @38
-  ++ @39 + kickout_3
-  ++ @40 + kickout_4
-END
-
-
-
-/* kicked out somewhere else (not bd4700.are in Avernus, not Korlasz's tomb) */
-IF ~GlobalGT("bd_joined","locals",0)~ THEN BEGIN kickout_5
-  SAY @43
-  + ~GlobalGT("bd_npc_camp_chapter","global",1)
-     GlobalLT("bd_npc_camp_chapter","global",5)
-     OR(2)
-       !Range("ff_camp",999)
-       NextTriggerObject("ff_camp")
-     !IsOverMe("X3mily")~ + @44 DO ~SetGlobal("bd_npc_camp","locals",1)~ + kickout_6
-  ++ @45 + kickout_3
-  ++ @40 + kickout_4
-END
-
-
-
-IF ~~ THEN BEGIN kickout_6
-  SAY @46
-  IF ~~ THEN DO ~SetGlobal("bd_joined","locals",0)~ EXIT
-END
 
 /* join-up after leaving the group */
 IF ~OR(2)
